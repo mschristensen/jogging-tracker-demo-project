59a40e80cc8dba2504577aafa18066ccc539a29d
Mike Christensen <mike_christensen@btinternet.com>
2017-01-09 12:45:56 +0000

    user tests implemented, db dropped before running reading env vars from .env file in test folder

--- test/user.js
+++ test/user.js
@@ -1,12 +1,126 @@
+/* jshint expr: true */
+/* global before */
+/* global describe */
+/* global it */
+'use strict';
+
 const should = require('chai').should();
 const expect = require('chai').expect;
-const supertest = require('supertest');
-const api = supertest('http://localhost:3000');
+const api = require('supertest')('http://localhost:3000/api');
+const mongoose = require('mongoose');
+const nconf = require('nconf');
 
 module.exports = function() {
-  describe('UserTest', () => {
-    it('should ...', (done) => {
-      done();
+  // Load env vars from .env file into process.env
+  require('dotenv').load();
+  nconf.use('memory');
+  nconf.argv();
+  nconf.env();
+
+  describe('Users', () => {
+    // before running tests, empty the database
+    before(function (done) {
+      mongoose.connect(process.env.MONGOLAB_URI, function(err) {
+        if(err) throw err;
+        mongoose.connection.db.dropDatabase(function(err) {
+          if(err) throw err;
+          done();
+        });
+      });
+    });
+
+    let token;
+    it('should have invalid email, password and name', (done) => {
+      api.post('/user')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .send({
+          email: 'bademail',
+          password: 'pw',
+          name: JSON.stringify({ first: 'F', last: 'L' })
+        })
+        .expect(403)
+        .end((err, res) => {
+          expect(res.body.payload.InvalidArguments).to.be.an('array');
+          expect(res.body.payload.InvalidArguments).to.include('email');
+          expect(res.body.payload.InvalidArguments).to.include('password');
+          expect(res.body.payload.InvalidArguments).to.include('name.first');
+          expect(res.body.payload.InvalidArguments).to.include('name.last');
+          done();
+        });
+    });
+
+    it('should successfully create new user and return token', (done) => {
+      api.post('/user')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .send({
+          email: 'user@one.com',
+          password: 'password',
+          name: JSON.stringify({ first: 'User', last: 'One' })
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.payload).to.include.keys('token');
+          token = res.body.payload.token;
+          done();
+        });
+    });
+
+    it('should successfully fetch users using token', (done) => {
+      api.get('/user')
+        .set('Authorization', token)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.payload[0]).to.include.keys('name');
+          expect(res.body.payload[0].name).to.include.keys('first');
+          expect(res.body.payload[0].name).to.include.keys('last');
+          expect(res.body.payload[0]).to.include.keys('email');
+          expect(res.body.payload[0]).to.include.keys('role');
+          expect(res.body.payload[0]).to.not.include.keys('password');
+          done();
+        });
+    });
+
+    it('should successfully login and receive new token', (done) => {
+      token = null;
+      api.post('/user/authenticate')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .send({
+          email: 'user@one.com',
+          password: 'password'
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.payload).to.include.keys('token');
+          token = res.body.payload.token;
+          done();
+        });
+    });
+
+    it('should successfully fetch users using token', (done) => {
+      api.get('/user')
+        .set('Authorization', token)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.payload[0]).to.include.keys('name');
+          expect(res.body.payload[0].name).to.include.keys('first');
+          expect(res.body.payload[0].name).to.include.keys('last');
+          expect(res.body.payload[0]).to.include.keys('email');
+          expect(res.body.payload[0]).to.include.keys('role');
+          expect(res.body.payload[0]).to.not.include.keys('password');
+          done();
+        });
+    });
+
+    it('should fail to fetch users with invalid token', (done) => {
+      // randomly shuffle the token characters (after the 'JWT ' head)
+      let shuffledToken = token.slice(4).split('').sort(function() { return 0.5 - Math.random(); }).join('');
+      api.get('/user')
+        .set('Authorization', shuffledToken)
+        .expect(403)
+        .end((err, res) => {
+          expect(res.body.payload).to.be.empty;
+          done();
+        });
     });
   });
 };
