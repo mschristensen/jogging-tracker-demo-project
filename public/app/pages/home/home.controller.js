1221cc47e7bab7dcc4abd02e7f10e69a0221e69a
Mike Christensen <mike_christensen@btinternet.com>
2017-01-13 11:47:54 +0000

    home state has open/closable menu

--- public/app/pages/home/home.controller.js
+++ public/app/pages/home/home.controller.js
@@ -2,5 +2,13 @@
 
 var app = angular.module('app');
 app.controller('homeController', ['$scope', function($scope) {
+  $scope.navOpen = false;
 
+  $scope.isNavOpen = function() {
+    return $scope.navOpen;
+  };
+
+  $scope.toggleNavOpen = function() {
+    $scope.navOpen = !$scope.navOpen;
+  };
 }]);
