8fcab644edb0f93284e5f986fbbb9b96691de849
Mike Christensen <mike_christensen@btinternet.com>
2017-01-14 12:33:59 +0000

    ensure unknown api endpoints return 404

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@
 const changeCase = require('change-case');
 const express = require('express');
 const routes = require('require-dir')();  // requires all other files in this directory
+const Response = require('../helpers/response.js');
 
 module.exports = function(app) {
   // Initialize all routes
@@ -17,5 +18,10 @@ module.exports = function(app) {
 
     // Tie the router to it's url path
     app.use('/api/' + changeCase.paramCase(routeName), router);
+  });
+
+  // Catch unknown endpoints as 404
+  app.all('/api/*', function(req, res, next) {
+    return Response.NotFound().send(res);
   });
 };
